95da943539eaef88925941800a88e6881f2f5782
Lyder Madsbu-Christensen <lyder.madsbu@gmail.com>
2023-10-30 10:34:15 +0100

    added fucked working ish folder and file making

--- code.cpp
+++ code.cpp
@@ -4,6 +4,18 @@
 using namespace std;
 #pragma warning(disable : 4996)
 
+class file {
+public:
+	string name;
+	int size;
+	int day;
+	int month;
+	int year;
+	int hour;
+	int min;
+
+};
+
 class folder {
 	public:
 		string name;
@@ -13,19 +25,20 @@ class folder {
 		int year;
 		int hour;
 		int min;
+		file files[10];
 };
-class file {
-	public:
-		string name;
-		int size;
-		int date;
-};
-int main() {
+
+int newfolder() {
 	time_t now = time(0);
 	tm* ltm = localtime(&now);
+	string foldername;
+	string filename;
+	cin >> foldername;
+	cout << "enter name of file in folder" << endl;
+	cin >> filename;
 
 	folder folder1;
-	folder1.name = "folder 1";
+	folder1.name = foldername;
 	folder1.size = 1 + (rand() % 100);
 	folder1.day = ltm->tm_mday;
 	folder1.month = 1 + ltm->tm_mon;
@@ -33,6 +46,29 @@ int main() {
 	folder1.hour = ltm->tm_hour;
 	folder1.min = ltm->tm_min;
 
-	cout << folder1.name << "   " << folder1.day << "." << folder1.month << "." << folder1.year << " " << folder1.hour << ":" << folder1.min << " " << folder1.size << "mb " << endl;
+	file file1;
+	file1.name = filename;
+	file1.size = 1 + (rand() % 100);
+	file1.day = ltm->tm_mday;
+	file1.month = 1 + ltm->tm_mon;
+	file1.year = 1900 + ltm->tm_year;
+	file1.hour = ltm->tm_hour;
+	file1.min = ltm->tm_min;
 
+	folder1.files[0] = file1;
+	cout << endl << foldername << ":" << endl;
+	for (int i = 0; i < 11; i++) {
+		cout << folder1.files[i].name << "   " << folder1.files[i].day << "." << folder1.files[i].month << "." << folder1.files[i].year << " " << folder1.files[i].hour << ":" << folder1.files[i].min << " " << folder1.files[i].size << "mb " << endl;
+	}
+	//cout << folder1.name << "   " << folder1.day << "." << folder1.month << "." << folder1.year << " " << folder1.hour << ":" << folder1.min << " " << folder1.size << "mb " << endl;
+	return 0;
+}
+int main() {
+	int answer;
+	cout << "make folder?" << endl << "enter 1 for yes" << endl;
+	cin >> answer;
+	if (answer == 1) {
+		newfolder();
+	}
+	main();
 }
